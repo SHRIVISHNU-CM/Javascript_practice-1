aa01fb550e12ada862209f3bc29170533a90eba9
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2023-08-01 19:39:35 +0530

    simple program using for loop

--- assign4.js
+++ assign4.js
@@ -1,3 +1,10 @@
 const child = 100;
 const adult = 150;
 const senior =120;
+
+let numberofchilds = 2;
+let numberofadults = 1;
+let numberofseniors = 1;
+
+let total = (child *numberofchilds + adult * numberofadults + senior * numberofseniors);
+console.log("the total ticket price is:" + total);
---
+++ assign7.js
@@ -0,0 +1,5 @@
+ let arr = ["apple", "onion", "dragon-fruit", "snacks","tomato","bananna","ice-cream"];
+
+ for ( let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+ }
