c813e163b278111d32e0d972aecc457bda76c41c
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2023-08-01 19:22:32 +0530

    simple login

--- assign.js
+++ assign.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+let username = "admin";
+let password = "12345";
+
+if(username == "admin" && password == "12345"){
+    console.log("login is successful");
+}else{
+    console.log("Please check email and password");
+}
